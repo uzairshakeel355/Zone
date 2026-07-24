d2271e726ae36c86fc6de1d62c0d705a3c06fd8b
Uzair Shakeel <uzair.shakeel355@gmail.com>
2026-07-24 05:04:19 +0500

    feat(server): add ApplicationDbContext with Identity Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShopZone.Api.Models;
+
+namespace ShopZone.Api.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
