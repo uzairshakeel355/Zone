8c76a6cdf317a71193670128d5929f32818c9a4f
Uzair Shakeel <uzair.shakeel355@gmail.com>
2026-07-26 20:30:21 +0500

    feat(server): add auth request/response DTOs

---
+++ DTOs/Auth/AuthDtos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopZone.Api.DTOs.Auth;
+
+public record RegisterDto(
+    [Required, EmailAddress] string Email,
+    [Required, MinLength(8)] string Password,
+    [Required] string FirstName,
+    [Required] string LastName
+);
+
+public record LoginDto(
+    [Required, EmailAddress] string Email,
+    [Required] string Password
+);
+
+public record AuthResponseDto(
+    string Token,
+    DateTime ExpiresAt,
+    string Email,
+    string FirstName,
+    string LastName,
+    IList<string> Roles
+);
