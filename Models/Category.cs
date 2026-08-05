894b22c8f4f2fb1f489b08467b9f8e8b0993f7eb
Uzair Shakeel <uzair.shakeel355@gmail.com>
2026-08-05 19:55:13 +0500

    feat(server): add Category entity

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopZone.Api.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Product> Products { get; set; } = new List<Product>();
+}
