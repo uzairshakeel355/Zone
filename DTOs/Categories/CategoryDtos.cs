42219086bff8bce804e54a140af8e310d4cf942c
Uzair Shakeel <uzair.shakeel355@gmail.com>
2026-08-08 20:16:24 +0500

    feat(server): add category request/response DTOs

---
+++ DTOs/Categories/CategoryDtos.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopZone.Api.DTOs.Categories;
+
+public record CategoryResponseDto(int Id, string Name, int ProductCount);
+
+public record CreateCategoryDto([Required] string Name);
+public record UpdateCategoryDto([Required] string Name);
