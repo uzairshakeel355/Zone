e6c567764ef2e666585bf5ee523cbde73f95e302
Uzair Shakeel <uzair.shakeel355@gmail.com>
2026-08-27 19:31:44 +0500

    feat(server): add Order entity

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopZone.Api.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public ApplicationUser User { get; set; } = null!;
+
+    public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    public OrderStatus Status { get; set; } = OrderStatus.Pending;
+    public string ShippingAddress { get; set; } = string.Empty;
+    public decimal TotalAmount { get; set; }
+
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
